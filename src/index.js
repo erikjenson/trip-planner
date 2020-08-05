ca6c0e77d5365e4c4e6342a5b4a4ce588d075915
Erik Jenson <erik.a.jenson@gmail.com>
2020-08-05 17:24:33 -0400

    added marker

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 console.log('Hello World');
 const mapboxgl = require('mapbox-gl');
+const { buildMarker, marker } = require('./marker');
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoiZWFqOSIsImEiOiJjazM0bzBoc3UwMHNhM2xteW9odTIwcmM1In0.edEo4d5VlgejuN584kbuaQ';
-
-const marker = require('./marker');
-
 const map = new mapboxgl.Map({
   container: 'map',
   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
   zoom: 12, // starting zoom
-  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
+  style: 'mapbox://styles/mapbox/streets-v11', // mapbox has lots of different map styles available.
 });
 
-new mapboxgl.Marker(marker('Hotel')).setLngLat([-74.009, 40.705]).addTo(map);
+// const markerDomEl = document.createElement('div');
+// markerDomEl.style.width = '32px';
+// markerDomEl.style.height = '39px';
+// markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)';
+
+marker(map);
+
+buildMarker('Activity', [-74.001, 40.57], map);
+
+// const mark = new mapboxgl.Marker(marker('Hotel'));
+// mark.setLngLat([-74.009, 40.705]).addTo(map);
--- src/marker.js
+++ src/marker.js
@@ -1,28 +1,44 @@
-const mapbox = require('mapbox-gl');
+const mapboxgl = require('mapbox-gl');
 
 const iconURLs = {
   Activity: 'http://i.imgur.com/WbMOfMl.png',
   Hotel: 'http://i.imgur.com/D9574Cu.png',
   Restaurant: 'http://i.imgur.com/cqR6pUI.png',
 };
-
-function buildMaker(type, coord) {
-  const marker = document.createElement('div');
-  marker.style.width = '32px';
-  marker.style.height = '39px';
-
-  if (type === 'Activity') {
-    marker.style.backgroundImage = iconURLs[type];
-  }
-  if (type === 'Hotel') {
-    marker.style.backgroundImage = iconURLs[type];
-  }
-  if (type === 'Restaurant') {
-    marker.style.backgroundImage = iconURLs[type];
-  }
-
-  return new mapboxgl.Marker(marker).setLngLat(coord).addTo(map);
+function buildMarker(type, coord, map) {
+  return map.on('load', function () {
+    map.loadImage(iconURLs[type], function (error, image) {
+      if (error) throw error;
+      map.addImage(type, image);
+      map.addSource('point', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: coord,
+              },
+            },
+          ],
+        },
+      });
+      map.addLayer({
+        id: 'points',
+        type: 'symbol',
+        source: 'point',
+        layout: {
+          'icon-image': type,
+          'icon-size': 1,
+        },
+      });
+    });
+  });
 }
-//[-74.009, 40.705]
 
-module.exports = buildMaker;
+const marker = (map) =>
+  new mapboxgl.Marker().setLngLat([-74.009, 40.705]).addTo(map);
+
+module.exports = { buildMarker, marker };
